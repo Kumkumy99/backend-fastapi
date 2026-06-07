91f94a49393212f1c5c2205d2b4ed16e4862fdba
Kumkum Yadav <yadavkumkum9009@gmail.com>
2026-06-07 12:40:09 +0530

    Complete CRUD operations for Notes API

--- NotesAPP/app/routes/notes_routes.py
+++ NotesAPP/app/routes/notes_routes.py
@@ -1,13 +1,32 @@
 from fastapi import APIRouter
 from schema.note import Note
+
 from services.notes_services import (
     create_note,
-    get_notes
+    get_notes,
+    get_notes_by_id,
+    delete_note_by_id,
+    update_note
 )
-router=APIRouter()
+
+router = APIRouter()
+
 @router.post("/notes")
-def add_notes(note:Note):
+def add_notes(note: Note):
     return create_note(note)
+
 @router.get("/notes")
 def fetch_notes():
     return get_notes()
+
+@router.get("/notes/{id}")
+def get_note_by_id(id: str):
+    return get_notes_by_id(id)
+
+@router.delete("/notes/{note_id}")
+def delete_note(note_id: str):
+    return delete_note_by_id(note_id)
+
+@router.put("/notes/{note_id}")
+def edit_note(note_id: str, note: Note):
+    return update_note(note_id, note)
--- NotesAPP/app/services/notes_services.py
+++ NotesAPP/app/services/notes_services.py
@@ -15,5 +15,37 @@ def create_note(note: Note):
     notes.append(note.model_dump())   
     write_notes(notes)
     return note
+def get_notes_by_id(id):
+    notes=read_notes()
+    for note in notes:
+        if note["id"]==id:
+            return note
+    return {"error":"not found"}
+def delete_note_by_id(note_id: str):
+    notes = read_notes()
+    updated_notes = [
+        note for note in notes
+        if note["id"] != note_id
+    ]
+    if len(updated_notes) == len(notes):
+        return {"error": "Note not found"}
+    write_notes(updated_notes)
+    return {"message": "Note deleted"}
+def update_note(note_id: str, updated_note: Note):
+    notes = read_notes()
+
+    for i, note in enumerate(notes):
+        if note["id"] == note_id:
+
+            updated_data = updated_note.model_dump()
+            updated_data["id"] = note_id
+
+            notes[i] = updated_data
+
+            write_notes(notes)
+
+            return updated_data
+
+    return {"error": "Note not found"}
 
 
